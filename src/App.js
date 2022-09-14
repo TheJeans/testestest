bfd7c822f208dc436ab4fb0411eaa2030e181ada
TheJeans <48654987+TheJeans@users.noreply.github.com>
2022-09-14 10:48:33 -0400

    yep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { createTheme, ThemeProvider } from 'design-system';
+// import {Button} from 'design-system';
+import {Checkbox} from 'design-system';
 
+const innerTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#00FF00',
+    },
+  },
+});
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={innerTheme}>
+        <Checkbox defaultChecked />
+      </ThemeProvider>
   );
 }
 
